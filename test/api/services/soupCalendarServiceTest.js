7ca11990cc90dab32b64f5dd1e7d2eb19370f874
Brandon Groff <mynamesbg@gmail.com>
2017-08-26 23:42:25 -0400

    Add failing test placeholder

---
+++ test/api/services/soupCalendarServiceTest.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const assert = require('assert');
+const sinon = require('sinon');
+const fs = require('fs');
+const path = require('path');
+const request = require('request');
+const moment = require('moment');
+const soupCalendarService = require('../../../api/services/soupCalendarService');
+
+describe('soupCalendarService', () => {
+  assert(false, 'testing TODO');
+});
