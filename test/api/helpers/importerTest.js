3bb70a29abe68001274c7546be46c57cd5303cd1
Brandon Groff <mynamesbg@gmail.com>
2019-03-17 19:54:00 -0400

    add logic for trying to parse all pages of pdf

--- test/api/helpers/importerTest.js
+++ test/api/helpers/importerTest.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const _ = require('lodash');
+const sinon = require('sinon');
 
 const importer = require('../../../api/helpers/importer');
 const sampleJson = require('../../data/calendar-pdf-json');
+const sampleJson2 = require('../../data/calendar-pdf-json-v2');
 
 const TEST_URL = 'https://www.subitosoups.com/s/2018-August-Menu.pdf';
 
@@ -50,6 +52,16 @@ describe('importer', () => {
       output = importer.extractFromPdf(null);
       assert.deepEqual(output, []);
     });
+
+    it('should allow a page number to be specified', () => {
+      const sample = _.cloneDeep(sampleJson);
+      sample.formImage.Pages[1] = sample.formImage.Pages[0];
+
+      const output = importer.extractFromPdf(sample, 1);
+
+      assert(_.every(output, _.isString), 'all rows should be strings');
+      assert.equal(output.length, 148);
+    });
   });
 
   describe('aggregateRows', () => {
@@ -66,6 +78,26 @@ describe('importer', () => {
     it('should return empty array when not in expected structure', () => {
       const rows = importer.aggregateRows(['hello world']);
       assert.equal(rows.length, 0);
+    });
+  });
+
+  describe('tryExtractRows', () => {
+    before(() => {
+      sinon.spy(importer, 'extractFromPdf');
+      sinon.spy(importer, 'aggregateRows');
+    });
+
+    after(() => {
+      importer.extractFromPdf.restore();
+      importer.aggregateRows.restore();
+    });
+
+    it('should extractFromPdf and aggregateRows until rows are found on a page', () => {
+      // valid data on page 2 for v2 sample
+      const rows = importer.tryExtractRows(sampleJson2);
+      rows.should.have.lengthOf(24);
+      sinon.assert.calledTwice(importer.extractFromPdf);
+      sinon.assert.calledTwice(importer.aggregateRows);
     });
   });
 });
